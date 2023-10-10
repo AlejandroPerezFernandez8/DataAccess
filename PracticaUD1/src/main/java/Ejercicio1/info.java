eac7509c4e868dd88e020ba426ba8030ff74c0d4
Alejandro Pérez Fernández <alejandro.perezferna@wirtz.local>
2023-10-10 16:19:03 +0200

    Practica final UD1

---
+++ PracticaUD1/src/main/java/Ejercicio1/info.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio1;
+
+/**
+ *
+ * @author alejandro.perezferna
+ */
+public class info {
+    String nombre,tipo;
+
+    public info(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
---
+++ PracticaUD1/src/main/java/Ejercicio1/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio1;
+
+import java.io.File;
+
+/**
+ *
+ * @author alejandro.perezferna
+ */
+public class main {
+    public static void main(String[] args) {
+        //SE RECORRERA TODA LA CARPETA EJERCICIO1 DE RESOURCES
+    }
+    
+    public void recorrerListado(File fichero){
+        
+    }
+    
+    
+}
