5b305490dafd4f8eb58d389f334ac937398429b1
Alejandro Pérez Fernández <134549761+AlejandroPerezFernandez8@users.noreply.github.com>
2023-10-15 01:46:10 +0200

    Proyecto Terminado

--- PracticaUD1/src/main/java/vista/Ejercicio1.java
+++ PracticaUD1/src/main/java/vista/Ejercicio1.java
@@ -6,15 +6,10 @@ package vista;
 
 import Modelo.info;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
 
 
@@ -23,68 +18,52 @@ import java.util.logging.Logger;
  * @author alejandro.perezferna
  */
 public class Ejercicio1 {
-    static File ficheroInicio = new File("./src/main/resources/Ejercicio1/");
-    static File ficheroObjetos = new File("./src/main/resources/Ejercicio1/info.dat");
+    
     public static void main(String[] args) {
-        ObjectInputStream x = null;
-        try {
-            ficheroObjetos.delete();
-            //SE RECORRERA TODA LA CARPETA EJERCICIO1 DE RESOURCES
-            recorrerListado(ficheroInicio);
-         
-            x = new ObjectInputStream(new FileInputStream (ficheroObjetos));
-            
-            System.out.println(x.readObject().getClass());
-            
-            
-            
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                x.close();
-            } catch (IOException ex) {
-                Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
+        File ficheroInicio = new File("./src/main/resources/Ejercicio1/");
+        ArrayList<info> ficheros = new ArrayList<>();
+        
+        new File("./src/main/resources/Ejercicio1/datos.dat").delete();
+        
+        recorrerListado(ficheroInicio, ficheros);
+        EscribirObjetos(ficheros);
+        
     }
 
-        public static void recorrerListado(File fichero) {
-        FilenameFilter filtro = (dir, name) -> !name.contains("NoCopiar");
+    public static void recorrerListado(File fichero,ArrayList<info> ficheros) {
+        FilenameFilter filtro = (dir, name) -> !name.equalsIgnoreCase("Nocopiar");
 
         File[] listadoFicheros = fichero.listFiles(filtro);
         
-        try(
-                ObjectOutputStream OIS = new ObjectOutputStream(new FileOutputStream(ficheroObjetos,true));
-           ){
+        
             for (File ficheroEncontrado : listadoFicheros) {
-                info info;
                 if (ficheroEncontrado.isDirectory()) {
-                    info = new info(ficheroEncontrado.getName(), "CARPETA");
-                    recorrerListado(ficheroEncontrado);
-                    System.out.println(info.toString());
-                    OIS.writeObject(info);
+                    info INFO= new info(ficheroEncontrado.getName(), "CARPETA");
+                    recorrerListado(ficheroEncontrado,ficheros);
+                    //System.out.println(INFO.toString());
+                    ficheros.add(INFO);
                 } else {
-                    info = new info(ficheroEncontrado.getName(), "FICHERO");
-                    System.out.println(info.toString());
-                    OIS.writeObject(info);
+                    info INFO = new info(ficheroEncontrado.getName(), "FICHERO");
+                    //System.out.println(INFO.toString());
+                    ficheros.add(INFO);
                 }
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println("FILE NOT FOUND");
-        } catch (IOException ex) {
-            System.out.println("IO EXCEPTION");
-        }
     }
         
+    
+    public static void EscribirObjetos(ArrayList<info> ficheros){
         
-       
-
-
+            try(
+                    FileOutputStream ficherosalida = new FileOutputStream(new File("./src/main/resources/Ejercicio1/datos.dat"));
+                    ObjectOutputStream OOS = new ObjectOutputStream(ficherosalida);
+               ) {
+                for (info fichero : ficheros) {
+                    OOS.writeObject(fichero);
+                }
+            } catch (Exception e) {
+                System.out.println("EXCEPCION EN LA ESCRITURA");
+            }
+        
+    }       
 }
     
--- PracticaUD1/src/main/java/vista/Ejercicio2.java
+++ PracticaUD1/src/main/java/vista/Ejercicio2.java
@@ -9,13 +9,8 @@ import Modelo.info;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -40,61 +35,36 @@ public class Ejercicio2 {
     }
     
     
-    private static void mostrarficheros(String modo) {
-        File ficheroObjetos = new File("./src/main/resources/Ejercicio1/info.dat");
-        
-         try (
-                FileInputStream FOS = new FileInputStream(ficheroObjetos);
-                ObjectInputStream OOS = new ObjectInputStream(FOS);
-            ){
-            info INFO = null; 
-             
-             while (true) {
+    public static void mostrarficheros(String modo) {
+        try (
+                FileInputStream ficherosalida = new FileInputStream(new File("./src/main/resources/Ejercicio1/datos.dat")); ObjectInputStream OIS = new ObjectInputStream(ficherosalida);) {
+            while (true) {
+                info INFO = (info) OIS.readObject();
 
-                 try {
-                     INFO = (info) OOS.readObject();
-
-                     if (INFO != null) {
-                         switch (modo) {
-                             case "f" -> {
-                                 if (INFO.getTipo().equalsIgnoreCase("FICHERO")) {
-                                     System.out.println("[" + INFO.getTipo() + "]" + INFO.getNombre());
-                                 }
-                             }
-                             case "d" -> {
-                                 if (INFO.getTipo().equalsIgnoreCase("CARPETA")) {
-                                     System.out.println("[" + INFO.getTipo() + "]" + INFO.getNombre());
-                                 }
-                             }
-                             case "t" -> {
-                                 System.out.println("[" + INFO.getTipo() + "]" + INFO.getNombre());
-                             }
-                             default ->
-                                 throw new AssertionError();
-                         }
-                     } else {
-                         break;  // Si INFO es null, salimos del bucle
-                     }
-                 } catch (EOFException e) {
-                     break;  // Se alcanzó el final del archivo
-                 }
-
-
+                switch (modo) {
+                    case "f" -> {
+                        if (INFO.getTipo().equalsIgnoreCase("FICHERO")) {
+                            System.out.println("[" + INFO.getTipo() + "]" + INFO.getNombre());
+                        }
+                    }
+                    case "d" -> {
+                        if (INFO.getTipo().equalsIgnoreCase("CARPETA")) {
+                            System.out.println("[" + INFO.getTipo() + "]" + INFO.getNombre());
+                        }
+                    }
+                    case "t" -> {
+                        System.out.println("[" + INFO.getTipo() + "]" + INFO.getNombre());
+                    }
+                    default ->
+                        throw new AssertionError();
+                }
             }
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException | ClassNotFoundException ex) {
-            Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (EOFException EOF) {
+            System.out.println("FIN DE FICHERO");
+        } catch (IOException ex) {
+            System.out.println("IO EXCEPTION");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Clase no encontrada");
         }
-    
     }
-    
-    
-            
-            
-        
-    }
-    
-
-
+}
