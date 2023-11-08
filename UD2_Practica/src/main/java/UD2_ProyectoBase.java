4c2137f9930371de5ed545db488eff42351dae94
Alejandro Pérez Fernández <Alejandro.perezferna@WIRTZ.LOCAL>
2023-11-08 17:56:19 +0100

    Creada la vista, añadido script de la BD

--- UD2_Practica/src/main/java/UD2_ProyectoBase.java
+++ UD2_Practica/src/main/java/UD2_ProyectoBase.java
@@ -1,4 +1,7 @@
 
+import controlador.controladorEjercicio1;
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
@@ -6,6 +9,6 @@
 public class UD2_ProyectoBase {
 
     public static void main(String[] args) {
-        
+        controladorEjercicio1.init();
     }
 }
---
+++ UD2_Practica/src/main/java/controlador/controladorEjercicio1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import vista.Ejercicio1;
+
+/**
+ *
+ * @author AD
+ */
+public class controladorEjercicio1 {
+    static Ejercicio1 ventana = new Ejercicio1();
+    
+    
+    public static void init(){
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+    
+    public static void iniciarFactory(){
+    
+    }
+    
+    public static void cerrarFactory(){
+    
+    }
+    
+    
+    
+    
+}
